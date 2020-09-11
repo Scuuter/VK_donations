d5847078fceb6f565193865056ddde4e476f14b4
Scuuter <class.object@yandex.ru>
2020-09-11 22:05:18 +0300

    rename toolbar

--- app/src/main/java/natasha/vk/donations/MainActivity.kt
+++ app/src/main/java/natasha/vk/donations/MainActivity.kt
@@ -2,11 +2,18 @@ package natasha.vk.donations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        val title = toolbar.findViewById<TextView>(R.id.activity_title)
+        title.text = "KEK"
     }
 }
