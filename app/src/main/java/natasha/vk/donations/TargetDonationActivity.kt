74315292311ba117e27067a6327e399f3092d83b
Scuuter <class.object@yandex.ru>
2020-09-12 02:10:07 +0300

    upload image

--- app/src/main/java/natasha/vk/donations/TargetDonationActivity.kt
+++ app/src/main/java/natasha/vk/donations/TargetDonationActivity.kt
@@ -1,11 +1,15 @@
 package natasha.vk.donations
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import java.io.IOException
 
 class TargetDonationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +25,40 @@ class TargetDonationActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.btn_back)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val button = findViewById<Button>(R.id.next)
-        button?.setOnClickListener { startActivity(Intent(this, TargetDonationExtraActivity::class.java)) }
+        val buttonNext = findViewById<Button>(R.id.next)
+        buttonNext?.setOnClickListener { startActivity(Intent(this, TargetDonationExtraActivity::class.java)) }
 
+        val uploadImage: Button = findViewById(R.id.upload_image)
+        uploadImage.setOnClickListener {
+            val photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent, GALLERY_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        imageReturnedIntent: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent)
+        var drawable: Drawable? = null
+        val uploadImage: Button = findViewById(R.id.upload_image)
+        when (requestCode) {
+            GALLERY_REQUEST -> if (resultCode == Activity.RESULT_OK) {
+                val imageUri: Uri = imageReturnedIntent?.data ?: return
+                try {
+                    val inputStream = contentResolver.openInputStream(imageUri)
+                    drawable = Drawable.createFromStream(inputStream, imageUri.toString())
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                uploadImage.background = drawable
+            }
+        }
+    }
+
+    companion object {
+        private const val GALLERY_REQUEST = 1
     }
 }
