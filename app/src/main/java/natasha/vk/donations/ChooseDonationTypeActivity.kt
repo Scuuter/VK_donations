7db80504f0c271d08b3c2986e44e1be78407a40a
Scuuter <class.object@yandex.ru>
2020-09-11 22:23:15 +0300

    second activity

--- app/src/main/java/natasha/vk/donations/ChooseDonationTypeActivity.kt
+++ app/src/main/java/natasha/vk/donations/ChooseDonationTypeActivity.kt
@@ -1,23 +1,18 @@
 package natasha.vk.donations
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class ChooseDonationTypeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.collection_type)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         val title = toolbar.findViewById<TextView>(R.id.activity_title)
-        title.text = getString(R.string.fundraising_title)
-
-        val button = findViewById<Button>(R.id.create_donation_button)
-        button.setOnClickListener { startActivity(Intent(this, ChooseDonationTypeActivity::class.java)) }
+        title.text = getString(R.string.choose_donation_type_title)
     }
 }
